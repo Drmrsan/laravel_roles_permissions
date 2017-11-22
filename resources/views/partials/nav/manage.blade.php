793f15ea0d8cb4f7eb7141d0be7a956b5a98af0a
Drmrsan <drmrsan@gmail.com>
2017-11-22 16:01:57 +0100

    Laratrust setup

---
+++ resources/views/partials/nav/manage.blade.php
@@ -0,0 +1,11 @@
+
+<ul class="nav nav-sidebar">
+	
+	<li>General</li>
+	<li><a href="">Dashboard</a></li>
+
+	<li>Administration</li>
+	<li><a href="">Manage Users</a></li>
+	<li><a href="">Roles &amp; Permissions</a></li>
+
+</ul>
